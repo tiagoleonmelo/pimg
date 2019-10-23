0eed4e569c3917221aa87e5711dca27e88d0ee5a
Tiago Melo <tleonmelo@hotmail.com>
2019-10-23 10:40:57 +0100

    Correct greyscale conversion and folder structure

---
+++ lib/formats.h
@@ -0,0 +1,107 @@
+/**
+ * 
+ * Header that contains DynMat functions and the pixel formats used
+ * to store and manipulate images.
+ * 
+ */
+
+#ifndef INC_00_FORMATS_H
+#define INC_00_FORMATS_H
+
+/**
+ * 
+ * RGB Pixel, used to read an Image in RGB
+ * Contains 3 distinct values, one for each color
+ * 
+ */
+typedef struct{
+    unsigned char r,g,b;
+} RGBPx;
+
+
+/**
+ * 
+ * Greyscale Pixel, used to read an Image into Greyscale format
+ * Contains a single value, holding the "amount of grey" in that pixel
+ * 
+ */
+typedef struct{
+    unsigned char grey;
+} GPx;
+
+
+/**
+ * 
+ * Structure that holds the whole image. For each black pixel found,
+ * image will store it using masks. The image can later be loading with a
+ * XOR operation
+ * 
+ */
+typedef struct{
+    unsigned long long image; 
+} Bitmap;
+
+
+
+/**
+ * 
+ * RGB Matrix that can grow dynamically, based on the provided
+ * MyVec implementation.
+ * 
+ */
+typedef struct {
+    RGBPx *data;
+    int n;
+    int x, y;
+    int size;
+    int max_bright;
+} DynamicMatrix;
+
+
+/**
+ * 
+ * GREY Matrix that can grow dynamically, based on the provided
+ * MyVec implementation.
+ * 
+ */
+typedef struct {
+    GPx *data;
+    int n;
+    int x, y;
+    int size;
+} GreyMatrix;
+
+
+
+
+// RGB Functions
+
+DynamicMatrix * CreateMat(int rows, int cols, int max_bright);
+
+DynamicMatrix * LoadFromFile(char *);
+
+void SaveOnFile(DynamicMatrix *v, char *name);
+
+void PrintMat(DynamicMatrix * dm);
+
+void PrintRGBPx(RGBPx * px);
+
+GreyMatrix * ConvertToGreyscale(DynamicMatrix * dm);
+
+GPx * ConvertPX(RGBPx * px);
+
+
+// Greyscale Functions
+
+GreyMatrix * CreateGreyMat(int rows, int cols);
+
+GreyMatrix * LoadGreyFromFile(char *);
+
+void SaveGreyOnFile(GreyMatrix *v, char *name);
+
+void PrintGreyMat(GreyMatrix * dm);
+
+void PrintGPx(GPx * px);
+
+
+#endif //INC_00_FORMATS_H
