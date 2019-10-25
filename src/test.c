274b3a5e28a4e16a3740df7ed35c7df71ca0f73c
Joao-Nogueira-gh <joaonogueira20@ua.pt>
2019-10-25 17:25:35 +0100

    retirar comments

--- src/test.c
+++ src/test.c
@@ -28,24 +28,24 @@ int main(void)
     // Saving it on a different file
     SaveOnFile(dm, "../res/savedRGB.ppm");
 
-    // // Converting the original RGB image to greyscale in each of the color channels
-    // // and storing the results (NONE means default RGB-Greyscale conversion)
-    // GreyMatrix * grey = ConvertToGreyscale(dm, NONE);
-    // SaveGreyOnFile(grey, "../res/grey_lena.pgm");
+    // Converting the original RGB image to greyscale in each of the color channels
+    // and storing the results (NONE means default RGB-Greyscale conversion)
+    GreyMatrix * grey = ConvertToGreyscale(dm, NONE);
+    SaveGreyOnFile(grey, "../res/grey_lena.pgm");
 
-    // GreyMatrix * red = ConvertToGreyscale(dm, RED);
-    // SaveGreyOnFile(red, "../res/red_lena.pgm");
+    GreyMatrix * red = ConvertToGreyscale(dm, RED);
+    SaveGreyOnFile(red, "../res/red_lena.pgm");
 
-    // GreyMatrix * green = ConvertToGreyscale(dm, GREEN);
-    // SaveGreyOnFile(green, "../res/green_lena.pgm");
+    GreyMatrix * green = ConvertToGreyscale(dm, GREEN);
+    SaveGreyOnFile(green, "../res/green_lena.pgm");
 
-    // GreyMatrix * blue = ConvertToGreyscale(dm, BLUE);
-    // SaveGreyOnFile(blue, "../res/blue_lena.pgm");
+    GreyMatrix * blue = ConvertToGreyscale(dm, BLUE);
+    SaveGreyOnFile(blue, "../res/blue_lena.pgm");
 
-    // // Loading GreyScale image //TODO: MAYBE USE A DIFFERENT IMAGE? mudei a ordem
-    // GreyMatrix * gm = LoadGreyFromFile("../res/grey_lena.pgm");
-    // // Saving it on a different file
-    // SaveGreyOnFile(gm, "../res/savedGrey.pgm");
+    // Loading GreyScale image //TODO: MAYBE USE A DIFFERENT IMAGE? mudei a ordem
+    GreyMatrix * gm = LoadGreyFromFile("../res/grey_lena.pgm");
+    // Saving it on a different file
+    SaveGreyOnFile(gm, "../res/savedGrey.pgm");
 
 
     // Accessing a single pixel in RGB format
@@ -58,15 +58,15 @@ int main(void)
     SaveOnFile(sub, "../res/cropped_lena.ppm");
 
 
-    // int threshold = 100;
-    // GreyMatrix * bmp = ConvertToBitGreyMat(gm, threshold);
-    // // PrintGreyMat(bmp);
-    // SaveGreyOnFile(bmp, "../res/bit_lena.pgm");
+    int threshold = 100;
+    GreyMatrix * bmp = ConvertToBitGreyMat(gm, threshold);
+    // PrintGreyMat(bmp);
+    SaveGreyOnFile(bmp, "../res/bit_lena.pgm");
 
 
-    // DynamicMatrix * saturated = Saturate(dm, -100);
-    // SaveOnFile(saturated, "../res/saturated_lena.ppm");
-    // // PrintMat(saturated);
+    DynamicMatrix * saturated = Saturate(dm, -100);
+    SaveOnFile(saturated, "../res/saturated_lena.ppm");
+    // PrintMat(saturated);
 
 
 
